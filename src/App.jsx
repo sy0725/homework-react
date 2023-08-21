ddb2c10059e0c9af861e0247f6eca400a6b73459
sy0725 <sunyong0725@naver.com>
2023-08-21 22:21:00 +0900

    Homework-2 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import Frist from "./layout/Frist";
-import Information from "./layout/Information";
-import HeaderBar from "./layout/HeaderBar";
-import Second from "./layout/Second";
-import Third from "./layout/Third";
-import FooterBar from "./layout/FooterBar";
+
 
 function App() {
   return (
-    <div className="w-[1680px] h-[2967px]">
-      <HeaderBar />
-      <Information />
-      <Frist />
-      <Second />
-      <Third />
-      <FooterBar />
+    <div >
+
     </div>
   );
 }
